44797251af04f4dda807d26f237e08712125e74d
BayLadyCoder <rachada_32@hotmail.com>
2024-06-21 18:23:36 -0400

    Add static product data

---
+++ products.js
@@ -0,0 +1,356 @@
+export const products = [
+  {
+    id: 1,
+    name: "Bananas",
+    category: "Produce",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Target-Ornament-Artificial-Bananas-Bunch-Lifelike-Banana-Cluster-Decoration-Fruits-Model-Photography-Props-Kitchen-Party_8af6613d-e32d-4818-8eba-b571fdef0ef0.2bd06d5ad1990818a1e5963a0dd91882.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/e6e55b3e-7573-4bf3-ab68-ec311d935d0a.d0f96a79a4a576b7f0a3582e79427ca1.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/7e532031-e661-4249-9a2d-0a813455205b.d2500f5272d93e4ec7184a2f0e17c279.jpeg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Watermelon",
+    category: "Produce",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Fresh-Seedless-Watermelon-Each_e2ec527d-fe7b-4309-9373-186de34557cf.1c562d1a69a2a8f4cb7b5de8f125fc76.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/7465add4-89cb-47d3-88ff-28d17f7c5a4c.e01b016179394177cf1b50ac0c2c7ead.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/a83e3e11-9128-4d98-8f6f-8c144e0d8e5e.a5fafdef89b7430bd13cae9037294d87.jpeg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Mango",
+    category: "Produce",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Fresh-Mangoes-Each-Sweet_cc54242f-cb87-4a25-9baa-fccaa20f5443.64fa79325ad44a7352dcd3c2a8b477be.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://walnutcreekfarmtexas.com/wp-content/uploads/2021/08/AdobeStock_76361909-scaled-1.jpeg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Apple",
+    category: "Produce",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Fresh-Honeycrisp-Apple-Each_fdffb720-8b75-43df-bffe-4ffc4e9369d8_3.d4460fa9146eb9716b5804d61d4b26b9.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/f2e5afeb-a01e-4f18-981e-f014bcfea62d_4.3a0df9d1b3bb552ed95762c46cb3ef3a.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/702be90b-5206-4976-a295-5d9958981971_3.8a72c4483e136753dafc359c5efad3cf.jpeg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Carrots",
+    category: "Produce",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/907ce2ec-4078-494f-8ac1-5e74ee211dd8.0d07225bf441585c4405db2a108a22eb.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Fresh-Whole-Carrots-1-lb-Bag_69764b97-0ef9-4f6e-b9ea-3e969b41f7db.e6c1d3c8a68365608dad0d1d8c689189.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/7f478d56-d3c2-42e8-afc9-e4c05d49a612.7688b55f5bceb709c8e425a42b52f95e.jpeg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Oatly Oatmilk",
+    category: "Dairy",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/ce42a44f-1471-49b5-8b4e-87e6a98055a8/fileType_jpg;size_600x600/ce42a44f-1471-49b5-8b4e-87e6a98055a8",
+      },
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/6cf107d9-e5f3-46e5-b86c-f78b80a787f7/fileType_jpg;size_600x600/6cf107d9-e5f3-46e5-b86c-f78b80a787f7",
+      },
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/248ab4a4-3d34-4b84-aee3-52dfe3ea25f4/fileType_jpg;size_600x600/248ab4a4-3d34-4b84-aee3-52dfe3ea25f4",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Silk Soy Milk",
+    category: "Dairy",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/9ce5fbae-71fa-4cbd-981f-028e9f71a821/fileType_jpg;size_600x600/9ce5fbae-71fa-4cbd-981f-028e9f71a821",
+      },
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/6c34a2ac-e062-400e-9d52-6b2c85c586d0/fileType_jpg;size_600x600/6c34a2ac-e062-400e-9d52-6b2c85c586d0",
+      },
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/ea078fcd-7c36-442b-bb03-9f29cba69df4/fileType_jpg;size_600x600/ea078fcd-7c36-442b-bb03-9f29cba69df4",
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Beyond Burger Patties",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/3cd85e9d-0e1d-4b12-b3bb-696946ca52c2/fileType_jpg;size_600x600/3cd85e9d-0e1d-4b12-b3bb-696946ca52c2",
+      },
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/90bc9830-1ac4-4b13-9cbb-fc98e6cb33aa/fileType_jpg;size_600x600/90bc9830-1ac4-4b13-9cbb-fc98e6cb33aa",
+      },
+      {
+        type: "image",
+        url: "https://media.post.rvohealth.io/wp-content/uploads/2019/06/Beyond_Burger_1200x628-facebook.jpg",
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Just Egg - Folded",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/b5af382b-89e2-4cf9-b1cf-9d68b23ccc94/fileType_jpg;size_600x600/b5af382b-89e2-4cf9-b1cf-9d68b23ccc94",
+      },
+      {
+        type: "image",
+        url: "https://m.media-amazon.com/images/I/A1GzXYkrdwL.jpg",
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: "Just Egg - Liquid",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/dc88523a-645c-4cd0-989a-7b4a492bfb2a/fileType_jpg;size_600x600/dc88523a-645c-4cd0-989a-7b4a492bfb2a",
+      },
+      {
+        type: "image",
+        url: "https://assets.ju.st/is/750x750/smart/filters:format(jpg):quality(80)/f/111747/3000x3000/895a063b75/1x1_just-egg-carton.jpg",
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: "Field Roast Hot Dogs",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://d2d8wwwkmhfcva.cloudfront.net/800x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_b244b836-e0a7-4172-a824-dc85a56d72f0.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://goodeggs4.imgix.net/13d341cf-f2f5-453c-8bb5-ca1ee92046b4.jpg?w=840&h=525&fm=jpg&q=80&fit=crop",
+      },
+      {
+        type: "image",
+        url: "https://m.media-amazon.com/images/I/81qYBHAObvL.jpg",
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "Lightlife Bacon",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://lightlife.com/wp-content/uploads/2023/04/106.png",
+      },
+      {
+        type: "image",
+        url: "https://goodeggs4.imgix.net/e16db6f6-e752-4ccf-bf45-0f510edc9533.jpg?w=840&h=525&fm=jpg&q=80&fit=crop",
+      },
+      {
+        type: "image",
+        url: "https://lightlife.ca/wp-content/uploads/2019/04/Lightlife_SmartBacon_FINAL_mobile.jpg",
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: "Follow Your Heart Mozzarella",
+    category: "Dairy",
+    media: [
+      {
+        type: "image",
+        url: "https://followyourheart.com/wp-content/uploads/2021/02/FYH_Shreds_Mozzarella_FinelyShredded_ProductPage.png",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/961630de-b56d-4774-90d7-2610009c31b4.85d8be76520e2fbc7f7e70b3c8a219b1.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/0e523dbc-1c69-45d2-af06-bf7c95611f6c.37ea05e95b85a66047f490190f9e2ce0.jpeg",
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: "Soon Veggie Ramen",
+    category: "Noodles",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Nongshim-Soon-Veggie-Savory-Vegan-Ramyun-Ramen-Noodle-Soup-Pack-3-95oz-X-4-Count_466022cf-4405-4587-be7a-018169943912.28acece645caa8b396ea7d8ce8ea9ac0.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Nongshim-Soon-Veggie-Savory-Vegan-Ramyun-Ramen-Noodle-Soup-Pack-3-95oz-X-4-Count_6bb657f3-2462-4cd9-9516-8dd604d147b1.818feb5c3e32d8e5f41f8d448029432b.jpeg",
+      },
+      {
+        type: "video",
+        url: "https://youtu.be/uEQ_espU39Q?si=6Ip4ruGCgN9rJ-aN",
+        embed:
+          '<iframe width="560" height="315" src="https://www.youtube.com/embed/uEQ_espU39Q?si=6Ip4ruGCgN9rJ-aN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: "Gardein Chicken Tender",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://www.gardein.com/sites/g/files/qyyrlu701/files/images/products/ultimate-plantbased-chickn-39623.png",
+      },
+      {
+        type: "image",
+        url: "https://scontent.fagc1-2.fna.fbcdn.net/v/t39.30808-6/244747668_10158648058660432_5197725633010416211_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=uGOyyz2v3SMQ7kNvgGzEuGc&_nc_ht=scontent.fagc1-2.fna&oh=00_AYBHcwh4R78Yadn1j0qAZNa-5bTTjFFMoUxxsadeZedIIA&oe=667BE032",
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: "Oatly Vanilla Ice Cream",
+    category: "Dessert",
+    media: [
+      {
+        type: "image",
+        url: "https://target.scene7.com/is/image/Target/GUEST_13bbeafa-9f51-4d95-bdb5-702b2b202a36?wid=488&hei=488&fmt=pjpeg",
+      },
+      {
+        type: "image",
+        url: "https://goodeggs4.imgix.net/be4fa657-64e0-4307-b5b2-1e243bf34dfc.jpg?w=840&h=525&fm=jpg&q=80&fit=crop",
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: "So Delicious Ice Cream Sandwiches",
+    category: "Dessert",
+    media: [
+      {
+        type: "image",
+        url: "https://assets.syndigo.cloud/cdn/59878455-bffa-4f3f-ac1a-9e3deb2df6dd/fileType_jpg;size_600x600/59878455-bffa-4f3f-ac1a-9e3deb2df6dd",
+      },
+      {
+        type: "image",
+        url: "https://godairyfree.org/wp-content/uploads/2013/08/product-sd-ice-cream-sandwiches.jpg",
+      },
+    ],
+  },
+  {
+    id: 18,
+    name: "Tofu",
+    category: "Protein",
+    media: [
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/seo/Nasoya-Refrigerated-Organic-Super-Firm-Tofu-16-oz_b130504e-277d-4e7e-b7a9-b1d42690da7a.7a9cc06c6bb178ef13a10f01452840c0.png",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/5b99f6fb-6ff7-4c3b-94b8-ea1bd85aa81e.c9a8f348eca688d1d0a756e5f9cccb84.jpeg",
+      },
+      {
+        type: "image",
+        url: "https://i5.walmartimages.com/asr/fd6fec8d-050b-477a-ac86-a2f5cccf3463.d431c12190218c153811b9ff571624b9.png?odnHeight=117&odnWidth=117&odnBg=FFFFFF",
+      },
+    ],
+  },
+  {
+    id: 19,
+    name: "Cheese Crackers",
+    category: "Snacks",
+    media: [
+      {
+        type: "image",
+        url: "https://m.media-amazon.com/images/I/81BvOW1w9ML.jpg",
+      },
+      {
+        type: "image",
+        url: "https://vegoutmag.com/wp-content/uploads/2024/03/Back-to-Nature.jpg",
+      },
+      {
+        type: "image",
+        url: "https://preview.redd.it/kg2kn9edzxj71.jpg?width=640&crop=smart&auto=webp&s=5e4b3e9bdb8432e44d76f2ff72f326dd63bd7c11",
+      },
+    ],
+  },
+  {
+    id: 20,
+    name: "Amy's Pad Thai",
+    category: "Noodles",
+    media: [
+      {
+        type: "image",
+        url: "https://storage.googleapis.com/images-lnb-prd-8936dd0.lnb.prd.v8.commerce.mi9cloud.com/product-images/detail/00042272009244.png",
+      },
+      {
+        type: "image",
+        url: "https://images.gopuff.com/blob/gopuffcatalogstorageprod/catalog-images-container/resize/cf/version=1_2,format=auto,fit=scale-down,width=800,height=800/7504ff11-4e41-4279-b43f-ee2dc93c68ef.png",
+      },
+    ],
+  },
+];
