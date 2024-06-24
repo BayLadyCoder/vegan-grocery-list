7fa045f7b84a862b95f420dd83eb84fb74788d6b
BayLadyCoder <rachada_32@hotmail.com>
2024-06-24 10:09:38 -0400

    Fix search products function

--- js/features/searchProducts.js
+++ js/features/searchProducts.js
@@ -3,13 +3,13 @@ import { renderProductCards } from "../utils/productHelpers.js";
 
 const searchProduct = (partialProductName) => {
   return products.filter((product) =>
-    product.name.includes(partialProductName)
+    product.name.toLowerCase().includes(partialProductName.toLowerCase())
   );
 };
 
 export const addSearchProductInputEventListener = () => {
-  const searchProductInput = document.querySelector("#search-product-input");
-  searchProductInput.addEventListener("change", (e) => {
+  const searchProductInput = document.getElementById("search-product-input");
+  searchProductInput.addEventListener("keyup", (e) => {
     const result = searchProduct(e.target.value);
     const productsContainer = document.querySelector(".products-container");
     productsContainer.replaceChildren();
